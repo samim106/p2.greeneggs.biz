a88d5ed86a87f72df0ff2f206173715e6fa658ad
Sam Im <samuelim@gmail.com>
2013-11-03 20:34:18 -0800

    p2 2013.11.03 2035

--- views/v_posts_index.php
+++ views/v_posts_index.php
@@ -9,7 +9,7 @@
 		</p>
 		<p class='article_poster'>
 			<?=substr($post['first_name'],0,1)?> <?=$post['last_name']?> - 
-			<time datetime="<?=Time::display($post['created'],'Y-m-d').'T'.Time::display($post['created'],'G:i')?>">
+			<time datetime="<?=Time::display($post['created'],'Y-m-d').'T'.Time::display($post['created'],'H:i:s')?>">
 				<?=Time::display($post['created'])?>
 			</time>
 		</p>
