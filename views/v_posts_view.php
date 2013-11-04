0e3562f1806a858fbbdccabfe59ddc4914f92fb6
Sam Im <samuelim@gmail.com>
2013-11-03 20:41:55 -0800

    p2 2013.11.03 2040

--- views/v_posts_view.php
+++ views/v_posts_view.php
@@ -1,9 +1,9 @@
 <div id='article_container'>
-	<article id='article_one'>
-		<p id='article_content'>
+	<article class='article_one'>
+		<p class='article_content'>
 			<?=$post['content']?>
 		</p>
-		<p id='article_poster'>Created: 
+		<p class='article_poster'>Created: 
 			<time datetime="<?=Time::display($post['created'],'Y-m-d G:i')?>">
 				<?=Time::display($post['created'])?>
 			</time>
